d2078e1b907e19b2e2577e58dc1be517c932ce32
douglasdwn <douglas_dwn@hotmail.com>
2018-03-31 15:17:14 -0300

    Express e Angular integrados

--- serve/app.js
+++ serve/app.js
@@ -22,7 +22,7 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 //app.use(express.static(path.join(__dirname, 'public')));
 
-//Diretorio do angular
+//Diretorio do Angular 5
 app.use(express.static(path.join(__dirname, '../dist/')));
 
 //app.use('/', index);
