6ee7a08c2bd5f3ecc7f852d13e1b6753f1dad91c
Andre Bronca <bronca.andre@gmail.com>
2018-03-31 17:30:19 -0300

    Módulo de lista de produtos

---
+++ src/app/produtos/produtos-lista.component.ts
@@ -0,0 +1,9 @@
+import { Component } from "@angular/core";
+
+
+@Component({
+  moduleId: module.id,
+  selector: 'produtos-lista',
+  templateUrl: 'produtos-lista.component.html'
+})
+export class ProdutosListaComponent {}
---
+++ src/app/produtos/produtos.module.ts
@@ -0,0 +1,11 @@
+import { NgModule } from '@angular/core';
+import { ProdutosListaComponent } from './produtos-lista.component';
+
+@NgModule({
+  declarations: [
+    ProdutosListaComponent
+  ]
+})
+export class ProdutosModule {
+
+}
