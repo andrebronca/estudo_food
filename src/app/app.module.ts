f62654c7d2ac79324cd572022e0f5f6fe168f423
Andre Bronca <bronca.andre@gmail.com>
2018-04-03 19:09:50 -0300

    criação do compoenent navbar. Ativação da exibição.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,11 +5,13 @@ import { NgModule } from '@angular/core';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { ProdutosModule } from './produtos/produtos.module';
+import { NavBarComponent } from './nav-bar/nav-bar.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NavBarComponent
   ],
   imports: [
     AppRoutingModule,
