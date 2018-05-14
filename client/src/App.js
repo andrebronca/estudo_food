faa8fbbab81353f21561b4dd4241b75399b3f3c0
douglasdwn <douglas_dwn@hotmail.com>
2018-05-14 00:06:23 -0300

    Axios - Api

--- client/src/App.js
+++ client/src/App.js
@@ -2,25 +2,34 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
+import Api from "./service/api.service";
+
 class App extends Component {
+  myApi = new Api({url : 'http://localhost:5000/api/v1'});
+  
   constructor(){
     super();
     this.state = { 
       'mesas': []
     };
+    //this.myApi;
   }
 
   componentDidMount(){
-    fetch('http://localhost:5000/api/v1/mesas')
-    .then(result =>{
-      return result.json();
-    })
-    .then(result =>{
-      this.setState({mesas : result});
+    // fetch('http://localhost:5000/api/v1/mesas')
+    // .then(result =>{
+    //   return result.json();
+    // })
+    // .then(result =>{
+    //   this.setState({mesas : result});
 
-      console.log(this.state.mesas);
-    })
-
+    //   console.log(this.state.mesas);
+    // })   
+    this.myApi.createEntity({name: 'mesas'});
+    this.myApi.endpoints.mesas.getAll()
+      .then(({data}) => {
+        this.setState({mesas: data});
+      })
   }
   render() {
     return (
